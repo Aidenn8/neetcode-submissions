f2986bfe4ff57c90368756491a432aea80493641
Aiden Huang <138057733+Aidenn8@users.noreply.github.com>
2026-05-16 11:24:05 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mps = {}
+        mpt = {}
+
+        if len(s) != len(t):
+            return False
+        
+
+        for i in range(len(s)):
+            if s[i] not in mps:
+                mps[s[i]] = 1
+            else:
+                mps[s[i]] += 1
+            if t[i] not in mpt:
+                mpt[t[i]] = 1
+            else:
+                mpt[t[i]] +=1
+        
+        return mps == mpt
+            
+        
+        
+
