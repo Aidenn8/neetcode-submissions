135bde20afd7fbd45ad368ba081c3cbbab348fbf
Aiden Huang <138057733+Aidenn8@users.noreply.github.com>
2026-05-25 14:43:17 -0700

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        //do thsi recursviely
+        //search all next islands
+        int total = 0;
+        for(int r = 0; r<grid.length; r++){
+            for(int c = 0; c<grid[0].length; c++){
+                if(grid[r][c] == '1'){
+                    search(grid, r, c);
+                    total+=1;
+                }
+
+            }
+        }
+        return total;
+    }  
+
+    public void search(char[][] grid, int r, int c){
+        if(r < 0 || c < 0 || r>grid.length-1 || c > grid[0].length-1){
+            return;
+        }
+        if(grid[r][c] == '1'){
+            grid[r][c] = '0';
+            search(grid, r+1, c);
+            search(grid, r, c+1);
+            search(grid, r-1, c);
+            search(grid, r, c-1);
+        }
+        
+
+    }
+}
+
