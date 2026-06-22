9482167671a5a59451b55cc5ed009b20640f0c07
Aiden Huang <138057733+Aidenn8@users.noreply.github.com>
2026-06-21 23:34:25 -0400

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = prices[0];
+        for(int i = 0; i<prices.length; i++){
+            max = Math.max(prices[i] - min, max);
+            min = Math.min(prices[i], min);
+        }
+        return max;
+    }
+}
